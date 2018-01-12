54f838223f691cb6aea3d71652967a82d4ae50b9
Jean-Francois Lentz <jean-francois.lentz@bnpparibasfortis.com>
2018-01-12 15:17:13 +0100

    start implementation of display in stdout

---
+++ pkg/grid/display.go
@@ -0,0 +1,19 @@
+package grid
+
+func Display(grid *Grid) {
+	var output = "+" + "---+" * columns + "\n"
+	//each_row do |row| top = "|" bottom = "+"
+	//row.each do |cell|
+	//	cell = Cell.new(-1, -1) unless cell
+	//body = " " # <-- that's THREE (3) spaces!
+	//east_boundary = (cell.linked?(cell.east) ? " " : "|")
+	//top << body << east_boundary
+	//# three spaces below, too >>-------------->> >...<
+	//south_boundary = (cell.linked?(cell.south) ? " corner = "+"
+	//bottom << south_boundary << corner
+	//end
+	//output << top << "\n"
+	//output << bottom << "\n" end
+	//output
+	//end
+}
